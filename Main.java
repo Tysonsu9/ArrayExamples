3e33dafee57f7d80aaaf195d2a8290e458e593d4
Tysonsu9 <43892770+Tysonsu9@users.noreply.github.com>
2018-10-17 12:26:13 -0700

    is this it ?
    hard to explain!

--- Main.java
+++ Main.java
@@ -149,7 +149,15 @@ class Main
          *          ar[3]=4
          */
         
-        for (int i = 0;
+        for (int i=0 ; i<ar1.length ; i++)
+        {
+            if ( ar1[i]%2==1)
+                ar1[i] *=10;
+        }
+        
+        for (int i=0 ; i<ar1.length ; i++)
+            System.out.println(ar1[i]);
+        
         
          /*
           * Task 8
@@ -161,6 +169,25 @@ class Main
           *    ar2[2]=2
           *    ar2[3]=3  ->  ar2odds[1]=3
           */
+         
+         int k=0;
+           int[] ar2odds = new int[ar2.length]; // too big right now.
+           for ( int i=0 ; i<ar2.length ; i++)
+           {
+               System.out.println(ar2[i]); // Just print it out to see
+               
+               if (i%2==1)
+               {
+                   ar2odds[k]=ar2[i];
+                   k++;
+               }
+           }
+           
+           for (int i=0 ; i<k ; i++)
+               System.out.println(ar2odds[i]);
+         
+         
+         
          
         /*
          * Task 9. In the array ar2, count how many odd numbers you
