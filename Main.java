a638292f73b56acd8d2747b9d27d88d3abe0fd17
Tysonsu9 <43892770+Tysonsu9@users.noreply.github.com>
2018-10-25 12:26:04 -0700

    yeah
    lol

--- Main.java
+++ Main.java
@@ -99,16 +99,7 @@ class Main
         
         
       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
+         /*
          * Task 6A. Print the 2nd to (n-1)th elements of ar1
          * Task 6B: Print out just the odd numbers in ar1
          * Task 6C: Print out the elements of ar1 when
@@ -281,6 +272,38 @@ class Main
          *  Count how many words have more than 5 letters.
          */
         
+          int len5;
+          int count5=0;
+          String s5="";
+          
+          ar5[0]="Four";
+          ar5[1]="score";
+          ar5[2]="and";
+          ar5[3]="seven";
+          ar5[4]="years";
+          ar5[5]="ago";
+          ar5[6]="our";
+          ar5[7]="fathers";
+          ar5[8]="brought";
+          ar5[9]="forth";
+          ar5[10]="on";
+          ar5[11]="this";
+          ar5[12]="continent";
+          ar5[13]="a";
+          ar5[14]="new";
+          ar5[15]="nation";
+          
+          for (int i=0; i<ar5.length; i++)
+          {
+            s5=ar5[i];
+            if((length5=s5.length()) >4);
+            count5++;
+            
+          }
+
+        
+        
+       
         /*
          * Task 13
          * Create an array called monsterArray of 5 Monsters.
