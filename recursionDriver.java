835148e8cf15efc21af54492dbc32fc16e0a017f
Tysonsu9 <43892770+Tysonsu9@users.noreply.github.com>
2018-11-06 12:25:02 -0800

    AYE
    LOL

--- recursionDriver.java
+++ recursionDriver.java
@@ -1,16 +1,9 @@
-public class recursionDriver
-{
- 
- public static int count(int n)
- {
-     
-  }
- 
- 
- 
- public static void main(String[] args)
- {
- /*
+
+
+
+
+
+/*
  * Count 
  * 1. Number of legs 10 elephant have
  * 2. Number of students in a school if each
@@ -18,20 +11,40 @@ public class recursionDriver
  * 3. Number of fingers if there are 8 people
  * 4. Find the sum of odd numbers from 0 to N
   * 5. Find the sum of even numbers from 0 to N
-  */ 
-   int counted=0;
-   // Task 1.
-   counted = count(10);
- System.out.println(counted);
+  */
  
- if (n=0)
+ public class recursionDriver
+{
+ 
+ 
+ public static int countNumbs(int n)
+  {
+   if (n<1)
+   {
    return 0;
-   return (count+4); 
-   
+   } 
+   return 4+countNumbs(n-1);
+  }
+ 
+ 
+ 
+ public static void main(String[] args)
+  {
+  int counted=0;
+  counted = countNumbs(12);
+  System.out.println(counted);
+  }
+ 
+ 
    // Task 2.
-   
-   
-   
+ public static int countStudents(int n)
+ {
+  if (n==0)
+  {
+  return 0;
+  }
+  return 20+countStudents(n-1);
+ }
  
  }
-}
+
